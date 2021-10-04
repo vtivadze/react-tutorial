26ea9f881b8b6e8ec4310c1e3f99b23f047ade76
Vakhtang Tivadze <vaxotivadze@gmail.com>
2021-10-04 21:14:19 +0400

    Pass 'handleSubmit' method to 'Form' component as props

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
         return (
             <div className="container">
                 <Table characterData={characters} removeCharacter={this.removeCharacter} />
-                <Form />
+                <Form handleSubmit={this.handleSubmit} />
             </div>
         );
     }
