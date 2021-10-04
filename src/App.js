8d46f733bd1497d2a37d1e6e9e7995869717921c
Vakhtang Tivadze <vaxotivadze@gmail.com>
2021-10-04 20:21:13 +0400

    Add 'removeCharacter' method to 'App' component

--- src/App.js
+++ src/App.js
@@ -23,9 +23,19 @@ class App extends Component {
         ],
     };
 
+    removeCharacter = (index) => {
+        const characters = this.state;
+
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i !== index;
+            }),
+        });
+    }
+
     render() {
         const {characters} = this.state;
-        
+
         return (
             <div className="container">
                 <Table characterData={characters} />
