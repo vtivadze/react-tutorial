7af020ab39dfaca116c07d1339634f90a5001011
Vakhtang Tivadze <vaxotivadze@gmail.com>
2021-10-04 19:54:00 +0400

    Add array of characters into 'App' component 'render' method

--- src/App.js
+++ src/App.js
@@ -3,6 +3,25 @@ import Table from './Table';
 
 class App extends Component {
     render() {
+        const characters = [
+            {
+                name: 'Charlie',
+                job: 'Janitor',
+            },
+            {
+                name: 'Mac',
+                job: 'Bouncer',
+            },
+            {
+                name: 'Dee',
+                job: 'Aspiring actress',
+            },
+            {
+                name: 'Dennis',
+                job: 'Bartender',
+            }
+        ];
+
         return (
             <div className="container">
                 <Table />
