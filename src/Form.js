1226023013d0e101d1dd784c749ccd7bb2cdc4c6
Vakhtang Tivadze <vaxotivadze@gmail.com>
2021-10-04 21:00:44 +0400

    Add 'handleChange' method to 'Form' component

--- src/Form.js
+++ src/Form.js
@@ -7,4 +7,12 @@ class Form extends Component {
     }
 
     state = this.initialState;
+
+    handleChange = (event) => {
+        const {name, value} = event.target;
+
+        this.setState({
+            [name]: value,
+        });
+    }
 }
