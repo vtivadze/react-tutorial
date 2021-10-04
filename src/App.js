da652ca054d6e271ddcc07f6a572e69226c66fd1
Vakhtang Tivadze <vaxotivadze@gmail.com>
2021-10-04 20:51:59 +0400

    Remove all the characters from the 'characters' array in 'App.js'

--- src/App.js
+++ src/App.js
@@ -3,24 +3,7 @@ import Table from './Table';
 
 class App extends Component {
     state = {
-        characters: [
-            {
-                name: 'Charlie',
-                job: 'Janitor',
-            },
-            {
-                name: 'Mac',
-                job: 'Bouncer',
-            },
-            {
-                name: 'Dee',
-                job: 'Aspiring actress',
-            },
-            {
-                name: 'Dennis',
-                job: 'Bartender',
-            }
-        ],
+        characters: [],
     };
 
     removeCharacter = (index) => {
