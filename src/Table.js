38cdedbb739f00ff446b4168416ab35f7cee57c9
Vakhtang Tivadze <vaxotivadze@gmail.com>
2021-10-04 19:50:42 +0400

    Remove data from 'TableBody' component

--- src/Table.js
+++ src/Table.js
@@ -13,20 +13,7 @@ const TableHeader = () => {
 
 const TableBody = () => {
     return (
-        <tbody>
-            <tr>
-                <td>Mac</td>
-                <td>Bouncer</td>
-            </tr>
-            <tr>
-                <td>Dee</td>
-                <td>Aspiring actress</td>
-            </tr>
-            <tr>
-                <td>Dennis</td>
-                <td>Bartender</td>
-            </tr>
-        </tbody>
+        <tbody />
     );
 }
 
