4cb0287cc11e2b4655045628513c8227d91de246
Vakhtang Tivadze <vaxotivadze@gmail.com>
2021-10-04 19:46:35 +0400

    Add 'TableHeader' and 'TableBody' React simple components to React class component 'Table'

--- src/Table.js
+++ src/Table.js
@@ -34,26 +34,8 @@ class Table extends Component {
     render() {
         return (
             <table>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Job</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Mac</td>
-                        <td>Bouncer</td>
-                    </tr>
-                    <tr>
-                        <td>Dee</td>
-                        <td>Aspiring actress</td>
-                    </tr>
-                    <tr>
-                        <td>Dennis</td>
-                        <td>Bartender</td>
-                    </tr>
-                </tbody>
+                <TableHeader />
+                <TableBody />
             </table>
         );
     }
