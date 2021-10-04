db123b5eb1c435c513105962c05715f309406478
Vakhtang Tivadze <vaxotivadze@gmail.com>
2021-10-04 21:06:07 +0400

    Add 'render' method to 'Form' component

--- src/Form.js
+++ src/Form.js
@@ -15,4 +15,29 @@ class Form extends Component {
             [name]: value,
         });
     }
+
+    render() {
+        const { name, job } = this.state;
+
+        return (
+            <form>
+                <label htmlFor="name">Name</label>
+                <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    value={name}
+                    onChange={this.handleChange} />
+                <label htmlFor="job">Job</label>
+                <input
+                    type="text"
+                    name="job"
+                    id="job"
+                    value={job}
+                    onChange={this.handleChange} />
+            </form>
+        );
+    }
 }
+
+export default Form;
